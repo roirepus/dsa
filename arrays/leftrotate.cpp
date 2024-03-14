58d7d8383cfcb019735277a7157e2703536d640f
roirepus <rajsinsinwar159@gmail.com>
2024-03-14 12:25:34 +0530

    array easy problems

--- arrays/leftrotate.cpp
+++ arrays/leftrotate.cpp
@@ -1,44 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-void leftRotate(int arr[],int n, int places){
-	int i=0;
-	for(int j=places;j<n;j++){
-		int temp = arr[i];
-		arr[i]=arr[j];
-		arr[j]=temp;
-		i++;
-		if(i>places){break;}
-	}
-	
-		int x= i;
-		for(int k=i;k<n;k++){
-			int swap = arr[x];
-			arr[x]=arr[k+1];
-			arr[k]=swap;
-			x++;
-		}
-
-}
-
-int main(){
-
-	cout<<"size:\n ";
-	int n;cin>>n;
-	int arr[n]={0};
-	for(int i=0;i<n;i++){
-		cin>>arr[i];
-	}
-
-	cout<<"enter places to rotate by: \n";
-
-	int places;cin>>places;
-	leftRotate(arr,n,places);
-
-	for(int i=0;i<n;i++){
-		cout<<arr[i]<<" ";
-	}
-
-	return 0;
+int main() {
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  int k;
+  cin >> k;
+  int temp[k];
+  for (int i = 0; i < k; i++) {
+    temp[i] = arr[i];
+  }
+  for (int i = 0; i < n - k; i++) {
+    arr[i] = arr[i + k];
+  }
+  for (int i = n - k; i < n; i++) {
+    arr[i] = temp[i - (n - k)];
+  }
+  for (auto x : arr) {
+    cout << x << " ";
+  }
 }
